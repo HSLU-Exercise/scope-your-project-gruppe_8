5df68d6c699c3468555f92ea42e22d759f72b133
alinafisc <alina.fischer@stud.hslu.ch>
2025-10-20 18:10:15 +0200

    Delete Security Policy Enforcement  directory

--- Security Policy Enforcement /Sicheres Dokument /secure_demo.py
+++
@@ -1,7 +0,0 @@
-# Sicheres Beispiel
-import getpass
-
-# Passwort wird sicher abgefragt
-user_password = getpass.getpass("Enter password: ")
-print("User logged in")
-# kein eval, kein hardcodiertes Passwort
--- Security Policy Enforcement /Unsichere Dokument /insecure_demo.py
+++
@@ -1,4 +0,0 @@
-# Unsicheres Beispiel
-password = "12345"  # Hardcoded password
-print("DEBUG: User logged in")
-eval("print('Unsicher!')")
